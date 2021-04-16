0410103ed40b96fe719c85ee14a7b57a10d52b7e
lxzh <leijinfeng@geetest.com>
2021-04-16 16:59:50 +0800

    对匹配的代码行号去重排序

--- sast_engine.py
+++ sast_engine.py
@@ -16,11 +16,11 @@ def scan(rule, extensions, paths, ignore_paths=None):
             'match_extensions': extensions,
             'ignore_paths': ignore_paths,
             'show_progress': False}
-        print("Scanner")
-        print(rule)
-        print(extensions)
-        print(ignore_paths)
-        print(paths)
+        # print("Scanner")
+        # print(rule)
+        # print(extensions)
+        # print(ignore_paths)
+        # print(paths)
         scanner = Scanner(options, paths)
         res = scanner.scan()
         if res:
@@ -34,31 +34,37 @@ def format_findings(findings, root):
     """Format findings."""
     for details in findings.values():
         tmp_dict = {}
+        print('**************************start')
+        print(details)
         for file_meta in details['files']:
-            file_meta['file_path'] = file_meta[
-                'file_path'].replace(root, '', 1)
+            file_meta['file_path'] = file_meta['file_path'].replace(root, '', 1)
             file_path = file_meta['file_path']
-            start = file_meta['match_lines'][0]
-            end = file_meta['match_lines'][1]
+            start     = file_meta['match_lines'][0]
+            end       = file_meta['match_lines'][1]
+            match_lines = []
             if start == end:
-                match_lines = start
+                match_lines.append(start)
             else:
                 exp_lines = []
                 for i in range(start, end + 1):
                     exp_lines.append(i)
-                match_lines = ','.join(str(m) for m in exp_lines)
+                # sort the matched line number
+                match_lines = exp_lines
             if file_path not in tmp_dict:
-                tmp_dict[file_path] = str(match_lines)
-            elif tmp_dict[file_path].endswith(','):
-                tmp_dict[file_path] += str(match_lines)
+                tmp_dict[file_path] = match_lines
             else:
-                tmp_dict[file_path] += ',' + str(match_lines)
+                tmp_dict[file_path].extend(match_lines)
         details['files'] = tmp_dict
+        print('**************************')
+        print(details)
+        print('**************************end')
     
     # sort the result with filepath
     for details in findings.values():
         tmp_dict = collections.OrderedDict()
         for key in sorted(details['files'].keys()):
-            tmp_dict[key] = details['files'][key]
+            tmp_dict[key] = str(sorted(list(set(details['files'][key]))))
         details['files'] =  tmp_dict
+    print(details)
+    print('=========================')
     return findings
